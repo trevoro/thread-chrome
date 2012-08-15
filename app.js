2b433f6592e5166b48de0b883a7f4c58b0f12a08
Trevor Orsztynowicz <trevoro@getebb.com>
2012-08-15 14:17:57 -0700

    Initial commit

--- app.js
+++ app.js
@@ -5,7 +5,10 @@
    TODO:
 
    * incorporate handlebars for easier templating
-   * gmail selectors for subject, from, date, content body, etc
+   * gmail selectors for from, date, content body, etc
+   * figure out how to scan a message body for dates
+   * better load_complete support for gmail (everything is async)
+   * try and get messageid from within gmail
 
 */
 
@@ -14,7 +17,8 @@ var options = {
   name: 'ebb',
   development: true,
   logLevel: 'debug',
-  apiHost: 'https://localhost:8080'
+  cssLink: 'styles/ebb.css',
+  apiHost: 'http://localhost:8080'
 }
 
 // --- Libraries 
@@ -44,7 +48,7 @@ Logger.prototype.level = function() {
   return this._level;
 }
 
-Logger.prototype.print = function(level, msg) {
+Logger.prototype._print = function(level, msg) {
   var toPrint = [
     this._name,
     (new Date).toJSON(),
@@ -64,16 +68,16 @@ Logger.prototype.print = function(level, msg) {
   }
 }
 
-Logger.prototype.info   = function(msg) { this.print('INFO',   msg); }
-Logger.prototype.debug  = function(msg) { this.print('DEBUG',  msg); }
-Logger.prototype.trace  = function(msg) { this.print('TRACE',  msg); }
-Logger.prototype.notice = function(msg) { this.print('NOTICE', msg); }
-Logger.prototype.warn   = function(msg) { this.print('WARN',   msg); }
-Logger.prototype.error  = function(msg) { this.print('ERROR',  msg); }
+Logger.prototype.info   = function(msg) { this._print('INFO',   msg); }
+Logger.prototype.debug  = function(msg) { this._print('DEBUG',  msg); }
+Logger.prototype.trace  = function(msg) { this._print('TRACE',  msg); }
+Logger.prototype.notice = function(msg) { this._print('NOTICE', msg); }
+Logger.prototype.warn   = function(msg) { this._print('WARN',   msg); }
+Logger.prototype.error  = function(msg) { this._print('ERROR',  msg); }
 
 var Gmail = function() {
-  this._name = 'gmail'; // not necessary
-
+  this.inboxLink = null;
+  
   if (!$) 
     throw new Error('Zepto must be initialized first');
 }
@@ -84,7 +88,14 @@ Gmail.prototype.threadId = function() {
 }
 
 Gmail.prototype.threadSubject = function() {
-  return 'subject';
+  return this._canvas()
+    .find('.hP')
+    //.filter(':visible') XXX need to build the selector module in zepto
+    .text();
+}
+
+Gmail.prototype.isThread = function() {
+  return (this.threadId() !== '#inbox')
 }
 
 Gmail.prototype._originalUrl = function(id) {
@@ -103,8 +114,30 @@ Gmail.prototype.loadOriginal = function(id, callback) {
   });
 }
 
-// Should probably be replaced with 
-// some kind of API Client function
+
+Gmail.prototype.createEvent = function(callback) {
+ // just posts the threadID to the appropriate API endpoint
+  var event = {
+    user_email: this.emailAddress(),
+    subject: this.threadSubject(),
+    thread_id: this.threadId(),
+    message_id: null,
+    inbound: true,
+    start_date: null,
+    duration: null,
+    contact_email: null
+  }
+
+  var response = JSON.stringify({event: event}, null, 2);
+  console.log(response);
+
+  $.post(options.apiHost + '/api/events.json', response, function(data) {
+    log.info('created event');
+    if (callback)
+      return callback(null)
+  });
+
+}
 
 Gmail.prototype.postOriginal = function(id, callback) {
   if (typeof(id) !== 'string') 
@@ -112,9 +145,16 @@ Gmail.prototype.postOriginal = function(id, callback) {
 
   this.loadOriginal(id, function(email) {
     var encoded = window.btoa(email);
-    //console.trace('data: ' + data);
     log.debug('encoded result length: ' + encoded.length);
     var result = { base64: encoded }
+
+    // Should probably be replaced with 
+    // some kind of API Client function
+    // client.createEvent(data, callback);
+    // client.updateEvent(id, data, callback);
+    // client.deleteEvent(id, callback);
+    // client.getEvent(id, callback);
+
     log.debug('POST /api/events.json');
     $.post(options.apiHost + '/api/events.json', 
       JSON.stringify({base64: encoded}),
@@ -150,33 +190,108 @@ Gmail.prototype.insertCss = function() {
   // do something
 }
 
-// --- Helpers
-//
+Gmail.prototype.currentView = function() {
+  if (this._canvas().find('h1.ha').length > 0) {
+    return 'conversation';
+  } else {
+    return 'thread';
+  }
+}
 
-var addDeveloperToolbar = function() {
-  log.info('adding devlopment toolbar');
+Gmail.prototype.banner = function() {
+  return this._canvas().find(['role=banner']);
+}
+
+Gmail.prototype.emailAddress = function() {
+  return document.title.split(/ - /)[1];
+}
+
+Gmail.prototype.numUnread = function() {
+  var title, num;
+
+  title = this.inboxUrl().title;
+  num = /\((\d+)\)/.exec(title);
+  return (num && num[1]) ? parseInt(num[1]) : 0;
+}
+
+Gmail.prototype._canvas = function() {
+  return $(document.getElementById('canvas_frame').contentDocument)
+}
+
+Gmail.prototype._body = function() {
+  return this._canvas()
+           .find('body')
+           .first();
+}
+
+Gmail.prototype.inboxUrl = function() {
+  var element, body, inbox;
+  
+  body= this._body();
+  element = body.find('a[href$="#inbox"][title^="Inbox"]');
+
+  if (element.length > 0)
+    inbox = element.first()[0];
+ 
+  return inbox;
+
+}
+
+Gmail.prototype.insertCss = function(csslink) {
+  var css = $('<link rel="stylesheet" type="text/css">');
+  css.attr('href', csslink);
+  this._canvas()
+    .find('head').first()
+    .append(css);
+}
+
+Gmail.prototype.addDeveloperToolbar = function() {
+  log.info('adding dev toolbar');
+  var self = this;
 
   var template = 
     [ '<div id="ebb">'
-    , '  <span>ebb</span>'
-    , '  <b>status:</b><span id="status">loaded</span>'
+    , '  <span><b>ebb</b></span>'
     , '  <span id="actions">'
     , '    <button id="submit">postOriginal</button>'
+    , '    <button id="debug">debug</button>'
+    , '    <button id="create_event">CREATE EVENT</button>'
     , '  </span>'
     , '</div'
     ].join('\n');
 
-  $(document.getElementById('canvas_frame').contentDocument)
-    .find('body').first()
+
+  var clickButton = function(selector, action) {
+    self._body().find(selector).on('click', action)
+  }
+
+  this._body()
     .prepend(template);
 
-  $(document.getElementById('canvas_frame').contentDocument)
-    .find('body').first()
-    .find('#ebb #actions #submit')
-    .on('click', function() {
-      id = gmail.threadId();
-      gmail.postOriginal(id);
-    });
+  clickButton('#ebb #actions #submit', function() {
+    id = gmail.threadId();
+    self.postOriginal(id);
+  });
+ 
+  clickButton('#ebb #actions #create_event', function() {
+    self.createEvent();
+  })
+
+  clickButton('#ebb #actions #submit', function() {
+    id = gmail.threadId();
+    self.postOriginal(id);
+  })
+
+  clickButton('#ebb #actions #debug', function() {
+    log.debug('printing debug output');
+    console.log('-------------------------------------------------');
+    console.log('emailAddress: ' + self.emailAddress());
+    console.log('numUnread: ' + self.numUnread());
+    console.log('inboxUrl: ' + self.inboxUrl());
+    console.log('threadId: ' + self.threadId());
+    console.log('threadSubject: ' + self.threadSubject());
+    console.log('-------------------------------------------------');
+  });
 
 }
 
@@ -187,16 +302,17 @@ var addDeveloperToolbar = function() {
 var log = new Logger({name: options.name, level: options.logLevel});
 var gmail = new Gmail();
 
-log.info('extension loaded');
+log.info('loaded');
 
 var main = function() {
+  gmail.insertCss(options.cssFile);
   if (options.development) {
-    addDeveloperToolbar();
+    gmail.addDeveloperToolbar();
   }
 
 } 
 
 Zepto(function($) {
   log.info('zepto loaded');
-  setTimeout(main, 1000); //XXX this needs to be more elegant
+  setTimeout(main, 3000); //XXX this needs to be more elegant
 });
